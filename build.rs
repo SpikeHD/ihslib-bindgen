7def338d12c12284e8040c8732e438b630a57229
SpikeHD <spikegdofficial@gmail.com>
2024-05-13 18:14:11 -0700

    fix: shift paths around

--- build.rs
+++ build.rs
@@ -10,13 +10,8 @@ fn main() {
         .generate()
         .expect("Unable to generate bindings");
 
-    // Create "dist"
-    if std::fs::create_dir("bindings").is_err() {
-        println!("bindings/ already exists");
-    }
-
     // Write the bindings to the dist folder
-    let out_path = PathBuf::from("./bindings");
+    let out_path = PathBuf::from("./src");
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
